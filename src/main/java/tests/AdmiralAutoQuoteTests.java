38d82f17d2874feea5787d8bcdb100474ef27504
nirmala <nirmalakuppusamy89@gmail.com>
2021-08-25 22:04:24 +0100

    Adding AdmiralAutoQuoteTests and Next-CountryDropDownTests

---
+++ src/main/java/tests/AdmiralAutoQuoteTests.java
@@ -0,0 +1,77 @@
+package tests;
+
+import functionLibrary.CommonFunctions;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdmiralAutoQuoteTests extends CommonFunctions {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(AdmiralAutoQuoteTests.class);
+
+    @Before
+    public void beforeTest()
+    {
+        openBrowser();
+        LOGGER.info("User opens admiral wesite");
+        driver.get("https://www.admiral.com");
+
+    }
+
+
+    @Test
+    public void verifyFindTheCarFeature()
+    {
+        LOGGER.info("clicking accept cookies button");
+        driver.findElement(By.id("ccc-recommended-settings")).click();
+
+        forceWaitForTime(3);
+
+        LOGGER.info("clicking the get a quote button");
+        driver.findElement(By.xpath("//a[contains(@class, 'button button--hero button--green')]")).click();
+
+        forceWaitForTime(10);
+        LOGGER.info("Verifing the header text");
+        String actualHeaderText = driver.findElement(
+                By.xpath("//*[@id=\"ng-app\"]/header/div[2]/h1")).getText();
+        Assert.assertTrue(actualHeaderText.contains("MultiCover Insurance"));
+
+        LOGGER.info("Clicking the registration number");
+        driver.findElement(By.xpath("//*[@id=\"registrationNr\"]")).getText();
+
+        LOGGER.info("Entering the car registration number");
+        driver.findElement(By.xpath("//input[@id='registrationNr']")).sendKeys("EY53 UOE");
+
+        LOGGER.info("Clicking the find car button");
+        driver.findElement(By.xpath("//button[contains(text(),'Find car')]")).click();
+        forceWaitForTime(3);
+        LOGGER.info("Getting registration number");
+        String actualRegistrationNumber = driver.findElement(By.xpath("//*[@id=\"ng-app\"]/div[2]/div/main/div[2]/div[2]/div/span/div/h3")).getText();
+        LOGGER.info("verifying the registration number");
+        Assert.assertTrue(actualRegistrationNumber.contains("EY53 UOE"));
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+    @After
+    public void afterTest()
+    {
+        closeBrowser();
+
+    }
+
+}
---
+++ src/main/java/tests/CountryDropDownTests.java
@@ -0,0 +1,54 @@
+package tests;
+
+import functionLibrary.CommonFunctions;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CountryDropDownTests extends CommonFunctions {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(CountryDropDownTests.class);
+
+    @Before
+    public void beforeTest()
+    {
+        openBrowser();
+        driver.get("https://www.next.co.uk/");
+
+    }
+    @Test
+    public void verifyUToSelectTheCountry() {
+
+        LOGGER.info("User clicks the headerFlagSelector");
+        driver.findElement(By.id("headerFlagSelector")).click();
+
+        forceWaitForTime(5);
+        LOGGER.info("User clicks the dropDown");
+        driver.findElement(By.xpath("//header/div[2]/section[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[1]/a[1]\n")).click();
+
+        forceWaitForTime(3);
+        LOGGER.info("User selects different country");
+        driver.findElement(By.xpath("//*[@id=\"dk_container_countrySelectorDropdown\"]/div/ul/li[72]/a")).click();
+
+        forceWaitForTime(2);
+        LOGGER.info("Clicking the shop now button");
+        driver.findElement(By.xpath("//*[@id=\"btnFlagSelectorShopNow\"]")).click();
+
+        LOGGER.info("Verifying the current Url = .us");
+        forceWaitForTime(3);
+        String actualCurrentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(actualCurrentUrl.contains(".us"));
+    }
+
+    @After
+    public void afterTest()
+    {
+        closeBrowser();
+
+    }
+
+}
