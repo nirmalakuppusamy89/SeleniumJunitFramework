787c096fd8c70484d07fcae57cba341fde3e44ee
nirmala <nirmalakuppusamy89@gmail.com>
2021-08-26 23:24:46 +0100

    Adding final logger

--- src/main/java/tests/SkyScannerFlightSearchTests.java
+++ src/main/java/tests/SkyScannerFlightSearchTests.java
@@ -45,6 +45,36 @@ public class SkyScannerFlightSearchTests extends CommonFunctions {
         Assert.assertEquals(expectedSearchText, actualSearchPageText);
 
     }
+    @Test
+    public void defaultHotelSearch()
+    {
+
+        LOGGER.info("Clicking cookies button");
+        driver.findElement(By.xpath("//*[@id=\"cookie-banner-root\"]/div[1]/div/div[2]/button[1]")).click();
+        forceWaitForTime(3);
+
+        LOGGER.info("User clicks the hotels option");
+        driver.findElement(By.xpath("//*[@id=\"skhot\"]")).click();
+        forceWaitForTime(3);
+
+        String expectedSearchText = "London";
+        LOGGER.info("Entering actual place of stay search text");
+        driver.findElement(By.xpath("//*[@id=\"destination-autosuggest\"]")).sendKeys(expectedSearchText);
+
+        forceWaitForTime(3);
+        LOGGER.info("User clicks search hotels button");
+        driver.findElement(By.xpath("//*[@id=\"search-controls\"]/div[2]/button")).click();
+        forceWaitForTime(10);
+
+        LOGGER.info("Getting hotel place search value");
+        String actualSearchPageText = driver.findElement(
+                By.xpath("//*[@id=\"searchResultCardsContainer\"]/div[1]/div/div[2]/p[1]")).getText();
+        forceWaitForTime(3);
+        LOGGER.info("Verifying the search result page text");
+        Assert.assertTrue(actualSearchPageText.contains(expectedSearchText));
+
+
+    }
 
     @After
     public void afterTest()
